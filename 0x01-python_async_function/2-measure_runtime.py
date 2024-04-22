7896aba6149ddf8a1fc1392415a3b8d61720ded7
oussamaNAJIH <oussamanajih6@gmail.com>
2024-04-22 12:55:44 +0200

    timeframe

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+the measure_time function module
+"""
+import asyncio
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    start_time: float = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end_time: float = time.time()
+    timeframe: float = end_time - start_time
+    return timeframe / n
