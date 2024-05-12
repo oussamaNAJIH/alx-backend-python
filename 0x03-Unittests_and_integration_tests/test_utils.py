47e1b4bd957c06eb389d6c5178f849662bfc26fb
oussamaNAJIH <oussamanajih6@gmail.com>
2024-05-12 22:23:26 +0200

    Mock HTTP calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -34,24 +34,24 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         self.assertRaises(expected, access_nested_map, nested_map, path)
 
-    class TestGetJson(unittest.TestCase):
+
+class TestGetJson(unittest.TestCase):
+    """
+    class for testing get_json function
+    """
+    @patch("utils.requests.get")
+    def test_get_json(self, mock_get_json):
         """
-        class for testing get_json function
+        Function for testing get_json function
         """
-        @patch("utils.requests.get")
-        @patch("utils.requests.get")
-        def test_get_json(self, mock_get_json):
-            """
-            Function for testing get_json function
-            """
-            test_cases = [
-                ("http://example.com", {"payload": True}),
-                ("http://holberton.io", {"payload": False})
+        test_cases = [
+            ("http://example.com", {"payload": True}),
+            ("http://holberton.io", {"payload": False})
             ]
-            for test_url, test_payload in test_cases:
-                mock_response = Mock()
-                mock_response.json.return_value = test_payload
-                mock_get_json.return_value = mock_response
-                result = get_json(test_url)
-                mock_get_json.assert_called_with(test_url)
-                self.assertEqual(test_payload, result)
+        for test_url, test_payload in test_cases:
+            mock_response = Mock()
+            mock_response.json.return_value = test_payload
+            mock_get_json.return_value = mock_response
+            result = get_json(test_url)
+            mock_get_json.assert_called_with(test_url)
+            self.assertEqual(test_payload, result)
